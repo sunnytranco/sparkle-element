1620a0eea9b12aa8437a8e7d7e469191bc149723
Sunny Tran <sunnytran.co@gmail.com>
2018-06-13 04:30:16 +0700

    Ready to diliver stage 1

--- src/App.vue
+++ src/App.vue
@@ -42,27 +42,63 @@
       </el-menu>
     </el-col>
 
-    <!-- Navbar -->
-    <el-col span="21">
-      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-        <el-menu-item index="1">Processing Center</el-menu-item>
-        <el-submenu index="2">
-          <template slot="title">Workspace</template>
-          <el-menu-item index="2-1">item one</el-menu-item>
-          <el-menu-item index="2-2">item two</el-menu-item>
-          <el-menu-item index="2-3">item three</el-menu-item>
-          <el-submenu index="2-4">
-            <template slot="title">item four</template>
-            <el-menu-item index="2-4-1">item one</el-menu-item>
-            <el-menu-item index="2-4-2">item two</el-menu-item>
-            <el-menu-item index="2-4-3">item three</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-menu-item index="3" disabled>Info</el-menu-item>
-        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
-      </el-menu>
+    <!-- Content -->
+    <el-col :span="12">
+      <el-form ref="form" :model="form" label-width="120px">
+        <el-form-item label="Activity name">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="Activity zone">
+          <el-select v-model="form.region" placeholder="please select your zone">
+            <el-option label="Zone one" value="shanghai"></el-option>
+            <el-option label="Zone two" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="Activity time">
+          <el-col :span="11">
+            <el-date-picker type="date" placeholder="Pick a date" v-model="form.date1" style="width: 100%;"></el-date-picker>
+          </el-col>
+          <el-col class="line" :span="2">-</el-col>
+          <el-col :span="11">
+            <el-time-picker type="fixed-time" placeholder="Pick a time" v-model="form.date2" style="width: 100%;"></el-time-picker>
+          </el-col>
+        </el-form-item>
+        <el-form-item label="Instant delivery">
+          <el-switch v-model="form.delivery"></el-switch>
+        </el-form-item>
+        <el-form-item label="Activity type">
+          <el-checkbox-group v-model="form.type">
+            <el-checkbox label="Online activities" name="type"></el-checkbox>
+            <el-checkbox label="Promotion activities" name="type"></el-checkbox>
+            <el-checkbox label="Offline activities" name="type"></el-checkbox>
+            <el-checkbox label="Simple brand exposure" name="type"></el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="Resources">
+          <el-radio-group v-model="form.resource">
+            <el-radio label="Sponsor"></el-radio>
+            <el-radio label="Venue"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="Activity form">
+          <el-input type="textarea" v-model="form.desc"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">Create</el-button>
+          <el-button>Cancel</el-button>
+        </el-form-item>
+      </el-form>
     </el-col>
     
+</el-row>
+<hr class="space">
+<el-row>
+  <el-button>Default</el-button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
+  <el-button type="warning">Warning</el-button>
+  <el-button type="danger">Danger</el-button>
 </el-row>
 
   </div>
@@ -72,6 +108,17 @@
 export default {
   data() {
       return {
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        currentDate: new Date(),
         activeIndex: '1',
         activeIndex2: '1'
       };
@@ -94,5 +141,39 @@ export default {
 #app {
   font-family: Helvetica, sans-serif;
   text-align: center;
+}
+
+.time {
+  font-size: 13px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+    display: table;
+    content: "";
+}
+
+.clearfix:after {
+    clear: both
+}
+
+.space {
+  margin: 100px auto;
 }
 </style>
